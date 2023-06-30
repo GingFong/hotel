a85f66259ff6d805239b153fe7da2292ef62ac95
kage <107029373+KageKing@users.noreply.github.com>
2023-06-30 12:24:07 +0200

    fix

--- reservatie_update.php
+++ reservatie_update.php
@@ -1,24 +1,28 @@
 <?php
-
 include "connect.php";
 
-$id = $_POST['id'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$room = $_POST['room'];
-$check_in = $_POST['check_in'];
-$check_out = $_POST['check_out'];
-$adults = $_POST['adults'];
-$children = $_POST['children'];
-$days = $_POST['days'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $room = $_POST['room'];
+    $check_in = $_POST['check_in'];
+    $check_out = $_POST['check_out'];
+    $adults = $_POST['adults'];
+    $children = $_POST['children'];
+    $days = $_POST['days'];
 
-$query = "UPDATE reservations SET name = ?, email = ?, room = ?, check_in = ?, check_out = ?, adults = ?  children = ? days = ? WHERE ID = ?";
+    $query = "UPDATE reservations SET name = ?, email = ?, room = ?, check_in = ?, check_out = ?, adults = ?, children = ?, days = ? WHERE id = ?";
 
-$sth = $pdo->prepare($query);
+    $sth = $pdo->prepare($query);
 
-try {
-    $sth->execute([$name, $email, $room, $check_in, $check_out, $adults, $children, $days, $id]);
-    echo "<script>alert('Klant Updated'); location.href = 'reservatie_bewerken.php'; </script>";
-} catch (PDOException $e) {
-    echo "<script>alert('Er is iets misgegaan')</script>";
+    try {
+        $sth->execute([$name, $email, $room, $check_in, $check_out, $adults, $children, $days, $id]);
+        echo "<script>alert('Klant Updated'); location.href = 'reservatie_bewerken.php'; </script>";
+    } catch (PDOException $e) {
+        echo "<script>alert('Er is iets misgegaan')</script>";
+    }
+} else {
+    echo "<script>alert('Invalid request')</script>";
 }
+?>
