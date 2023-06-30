6b9f5d2e599e1c3f1ac705f0a4a4b68e4a0eaa3b
kage <107029373+KageKing@users.noreply.github.com>
2023-06-30 12:57:04 +0200

    jjj

--- contactDB.php
+++ contactDB.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "contact";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($localhost, $root, $password, $contact);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
